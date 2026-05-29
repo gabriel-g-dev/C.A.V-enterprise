107973a76dbd1af739514dc631fcdfd390ff496e
Gabriel-hub-prog22 <rm563298@fiap.com.br>
2026-05-28 21:48:22 -0300

    chore: initial spring boot setup with dependencies for java advanced gs

---
+++ src/main/java/br/com/fiap/satguard/SatGuardApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.satguard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SatGuardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SatGuardApplication.class, args);
+	}
+
+}
